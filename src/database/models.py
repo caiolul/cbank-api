1192a304caedc0562416b00cd9586fad2507a875
caiolul <caiolucena.il@gmail.com>
2021-04-16 22:26:36 -0300

    Feat: Create models balance

--- src/database/models.py
+++ src/database/models.py
@@ -1,28 +1,54 @@
-import sqlalchemy
+# import base64
+# import twofish
+# import bcrypt
+# from twofish import Twofish
+
+from sqlalchemy import Table, Integer, MetaData, String, Column, ForeignKey, Float
+# from sqlalchemy.orm import relationship
 import databases
 from starlette.config import Config
 
 config = Config('.env')
 
-DATABASE_URL = 'sqlite:///test.db'
+DATABASE_URL = config("DATABASE_URL")
 
-metadata = sqlalchemy.MetaData()
+metadata = MetaData()
 
-User = sqlalchemy.Table(
+# relation = relationship()
+
+User = Table(
     'User',
     metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("email", sqlalchemy.String, unique=True, nullable=False),
-    sqlalchemy.Column("name", sqlalchemy.String, nullable=False),
-    sqlalchemy.Column("cpf", sqlalchemy.String, unique=True, nullable=False),
+    Column("id", Integer, primary_key=True),
+    Column("email", String, unique=True, nullable=False),
+    Column("name", String, nullable=False),
+    Column("cpf", String, unique=True, nullable=False),
+    Column("password", String, nullable=False),
+    # relationship("Balance", uselist=False, back_populates="User"),
+
+)
+Balance = Table(
+    'Balance',
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("value", Float, nullable=False),
+    Column("user_cpf", Integer, ForeignKey(
+        'User.cpf'), nullable=False, unique=True),
+    # relationship("User", back_populates="Balance"),
+
 )
 
-
-# class State(sqlalchemy):
-#     cnpj = sqlalchemy.Column(sqlalchemy.String(120), unique=True,
-#                              nullable=False, primary_key=True)
-#     username = sqlalchemy.Column(sqlalchemy.String(80), nullable=False)
-#     email = sqlalchemy.Column(sqlalchemy.String(120),  nullable=False)
-
+# password = bytes("klsjdlkajsdlkajlsdkj", "ascii")
+# # Hash a password for the first time, with a randomly-generated salt
+# hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+# # Check that an unhashed password matches one that has previously been
+# # hashed
+# ch = str(hashed, 'utf-8')
+# print(ch)
+# print(hashed)
+# if bcrypt.checkpw(password, hashed):
+#     print("It Matches!")
+# else:
+#     print("It Does not Match :(")
 
 database = databases.Database(DATABASE_URL)
