ecd3e2c6e92d8bc366f87d888a81f140d6ff5f5e
caiolul <caiolucena.il@gmail.com>
2021-04-16 22:27:36 -0300

    Feat: Create encrypt password and add balance to user

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,48 +1,58 @@
-from starlette.templating import Jinja2Templates
+import bcrypt
 from starlette.routing import Route, Mount, WebSocketRoute
 from starlette.responses import JSONResponse
 from src.socket.websocket import websocket_endpoint
-from src.database.models import User, database
+from src.database.models import User, Balance, database
+from src.utils.hash import encode
 
 
-templates = Jinja2Templates(directory='templates')
-
-
-async def homepage(request):
-    return templates.TemplateResponse('index.html', {'request': request})
-
-
-async def list_user(request):
+async def list_user(request) -> object:
     query = User.select()
     results = await database.fetch_all(query)
     content = [
         {
             "Name": result["name"],
             "Email": result["email"],
-            "Cpf": result["cpf"]
+            "Cpf": result["cpf"],
+            "Password": str(result["password"], 'utf-8')
         }
         for result in results
     ]
+    print(results)
+    # print(type(JSONResponse))
     return JSONResponse(content)
 
 
-async def add_user(request):
+async def add_user(request) -> object:
     data = await request.json()
-    query = User.insert().values(
+    hashed = bytes(data["password"], "ascii")
+    # fish = encode(data["password"])
+    # print(fish)
+    query_user = User.insert().values(
         name=data["name"],
         email=data["email"],
-        cpf=data["cpf"]
+        cpf=data["cpf"],
+        password=bcrypt.hashpw(hashed, bcrypt.gensalt())
     )
-    await database.execute(query)
+    add_balance = Balance.insert().values(
+        value=data["value"],
+        user_cpf=data["cpf"]
+    )
+
+    await database.execute(query_user)
+    await database.execute(add_balance)
     return JSONResponse({
         "Name": data["name"],
         "Email": data["email"],
-        "Cpf": data["cpf"]
+        "Cpf": data["cpf"],
+
+        "Account": data["value"]
+        # "Password": data["password"]
     })
 
 api_routes = [
-    Route('/', homepage),
+    # Route('/', homepage),
     Route("/user", endpoint=list_user, methods=["GET"]),
     Route("/user", endpoint=add_user, methods=["POST"]),
-    WebSocketRoute('/ws', websocket_endpoint)
+    # WebSocketRoute('/ws', websocket_endpoint)
 ]
