ba18b34653c28bab7f0b75f877bae92a6612bd53
caiolul <caiolucena.il@gmail.com>
2021-04-22 21:22:26 -0300

    Feat: Create middleware

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@ from starlette.applications import Starlette
 from starlette.routing import Route
 from src.routes.routes import api_routes
 from src.database.models import database
+from src.middleware.token_auth import middleware_token
 
 
 app = Starlette(debug=True, routes=api_routes, on_startup=[
-                database.connect], on_shutdown=[database.disconnect])
+                database.connect], on_shutdown=[database.disconnect], middleware=middleware_token())
---
+++ src/middleware/token_auth.py
@@ -0,0 +1,14 @@
+from starlette_jwt import JWTAuthenticationBackend
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.middleware import Middleware
+from starlette.config import Config
+
+key = Config('.env')
+
+
+def middleware_token():
+    back_token = JWTAuthenticationBackend(
+        secret_key=key('HASH_GEN'), prefix='JWT')
+
+    mid = [Middleware(AuthenticationMiddleware, backend=back_token)]
+    return mid
