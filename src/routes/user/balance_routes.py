b7a12a0d95abc815a82c6852e8850a0850e32a0b
caiolul <caiolucena.il@gmail.com>
2021-05-02 17:21:17 -0300

    Fix: Transferring to other user

--- src/routes/user/balance_routes.py
+++ src/routes/user/balance_routes.py
@@ -19,7 +19,10 @@ async def check_balance(request: Request) -> object:
     data: JWTUser = request.user
     # Query balance
     query = await query_balance(data.payload['cpf'])
-    return JSONResponse({"balance": query["value"]})
+    if query:
+        return JSONResponse({"balance": query["value"]})
+
+    return JSONResponse({"Balance not found": "check data entry"}, status_code=400)
 
 
 @requires('authenticated')
@@ -49,7 +52,7 @@ async def withdrawn(request: Request) -> object:
         # Return new value
         return JSONResponse({"New balance": result})
     # Return errors
-    return JSONResponse({"Error": "higher value than available"})
+    return JSONResponse({"Error": "higher value than available"}, status_code=400)
 
 
 @requires('authenticated')
@@ -76,7 +79,7 @@ async def deposit(request: Request) -> object:
         # Return new balance
         return JSONResponse({"New balance": result})
     # Return errors
-    return JSONResponse({"Error": "Value incorrect"})
+    return JSONResponse({"Error": "Value incorrect"}, status_code=400)
 
 
 @requires('authenticated')
@@ -94,11 +97,12 @@ async def transfer_to_cpf(request: Request) -> object:
     query_cpf = await query_balance(data_json["cpf"])
 
     new_balance = query_cpf["value"] + data_json["value"]
+    remove_balance = query["value"] - data_json["value"]
 
     # Handling request
-    if result >= 0.0 and query_cpf != None:
+    if result >= 0.0 and query_cpf != None and remove_balance >= 0.0:
         # Query to update balance
-
+        update_send_balance = await update_balance(remove_balance, data.payload["cpf"])
         resutl_query = await update_balance(new_balance, data_json["cpf"])
         # Create history
         history = History.insert().values(
@@ -110,6 +114,7 @@ async def transfer_to_cpf(request: Request) -> object:
 
         await database.execute(history)
         # Return new value
-        return JSONResponse({"New balance": new_balance})
+        return JSONResponse({"New balance recive": new_balance,
+                             "New balance send": remove_balance})
     # Return errors
-    return JSONResponse({"Error": "higher value than available"})
+    return JSONResponse({"Error": "higher value than available"}, status_code=400)
